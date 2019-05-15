5f8124d321a99bdc2df2231712bac7a8381eb952
Subhadip Dey <onesubhadip@gmail.com>
2019-05-15 23:24:23 +0530

    create spring boot app for task list REST APIs

---
+++ task-list-api/src/main/java/org/onesubhadip/tasklistapi/TaskListApiApplication.java
@@ -0,0 +1,13 @@
+package org.onesubhadip.tasklistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListApiApplication.class, args);
+	}
+
+}
